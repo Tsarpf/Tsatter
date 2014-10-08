2220940ebf96464e5b2f28ff048b0622a8f0d4c0
Tsarpf <tsarpf@gmail.com>
2014-10-09 02:26:37 +0300

    Forgot nodejs server code

---
+++ main.js
@@ -0,0 +1,38 @@
+var ws = require("nodejs-websocket");
+
+
+var count = 0;
+var server = ws.createServer(function (conn) {
+    count++;
+    var user = "anon" + count;
+    console.log("new connection");
+    broadcast(user + " joined chat");
+
+    conn.on("text", function(str) {
+        console.log("received: (" + str + ")");
+        broadcast(user + ": " + str);
+    });
+
+    conn.on("close", function (code, reason) {
+        console.log("connection closed");
+    });
+    conn.on("error", function(err) {
+        console.log("Error: " + err);
+        broadcast(user + ": " + error);
+    });
+
+    conn.on("close", function () {
+        var msg = user + ": closed connection.";
+        console.log(msg);
+        broadcast(msg);
+    });
+
+
+});
+server.listen(8010);
+
+function broadcast(msg) {
+    server.connections.forEach(function (conn) {
+        conn.sendText(msg);
+    });
+}
