f3b7671315017fbb98900a3cf1425bc9a1596d7a
Tsarpf <tsarpf@gmail.com>
2014-10-26 03:56:29 +0300

    Added test test

---
+++ test/test.js
@@ -0,0 +1,25 @@
+var should = require('should');
+
+describe('hooks', function() {
+  before(function() {
+    // runs before all tests in this block
+  })
+  after(function(){
+    // runs after all tests in this block
+  })
+  beforeEach(function(){
+    // runs before each test in this block
+  })
+  afterEach(function(){
+    // runs after each test in this block
+  })
+  // test cases
+
+    it('should fail?', function() {
+        [1,2,3][0].should.equal(-1);
+    });
+
+    it('should pass', function() {
+        [1,2,3][0].should.equal(1);
+    });
+})
