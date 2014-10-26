cd18c2642220e5736ad198595574c6f5e49ccca2
Tsarpf <tsarpf@gmail.com>
2014-10-26 05:42:18 +0200

    Testing 3

--- tsatterServer.js
+++ tsatterServer.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send("Hello world! modified");
+    res.send("Hello world! modified 2");
 });
 
 var server = app.listen(7247, function() {
