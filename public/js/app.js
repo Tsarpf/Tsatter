6ab598be013b8843e23e9357343b12f7b41e2fef
Tsarpf <tsarpf@gmail.com>
2014-10-28 01:25:00 +0200

    Room name is now supplied as an attribute to the ts-chat directive. Code atm pretty messy and incomplete. Not long before it's time to add websockets though. yay\!

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,21 @@ var app = angular.module('tsatter', []);
 
 //app.controller('ChatController', function() {
 app.controller('ChatController', ['$scope', function($scope) {
-    $scope.test = 1;
+    this.test = 1;
+    this.addOne = function() {
+        this.test++;
+    }
 }]);
 
 app.directive('tsChat', function() {
     return {
         restrict: "E",
-        templateUrl: '/partials/chat'
+        templateUrl: '/partials/chat',
+        link: function(scope, element, attrs) {
+            console.log(attrs);
+            scope.roomName=attrs.roomName;
+            scope.test = parseInt(attrs.roomName);
+            console.log(scope.roomName);
+        }
     };
 });
