dcbce7ddbbef184f6e4eead48721fb1b85e05ac4
Tsarpf <tsarpf@gmail.com>
2014-11-03 13:03:21 +0200

    Refactored socket code to it's own file

---
+++ sockets.js
@@ -0,0 +1,60 @@
+
+
+var channels = {}
+
+var isOnChannel = function(user, channel) {
+
+}
+var joinChannel = function(user, channel, socket) {
+    if(!channels[channel]){
+        channels[channel] = {};
+        io.sockets.emit('hello', {channels: channels});
+    }
+    channels[channel][user] = "ses";
+    socket.join(channel);
+}
+var leaveChannel = function(user, channel) {
+}
+
+var count = 0;
+
+var initializeConnections = function(socket) {
+    count++;
+    var user = 'anon' + count;
+    socket.emit('hello', {channels: channels});
+    socket.on('join', function(data, fn) {
+        if(data.room) {
+            try {
+                joinChannel(user, data.room, socket);
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+        fn('joined');
+    });
+    socket.on('leave', function(data) {
+        if(data.room) {
+            try {
+                socket.leave(data.room);
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+    });
+    socket.on('message', function(data) {
+        if(channels[data.room] && channels[data.room][user]){
+            io.to(data.room).emit(data.room, {user: user, message: data.message});
+        }
+        else {
+            console.log("error:");
+            console.log(data);
+        }
+    });
+    
+}
+
+module.exports = {
+    initCons: initializeConnections
+}
--- tsatterServer.js
+++ tsatterServer.js
@@ -44,60 +44,9 @@ var io = require('socket.io')(server);
 //routes
 require('./routes')(app);
 
-var channels = {}
+var initCons = require('./sockets').initCons;
 
-var isOnChannel = function(user, channel) {
-
-}
-var joinChannel = function(user, channel, socket) {
-    if(!channels[channel]){
-        channels[channel] = {};
-        io.sockets.emit('hello', {channels: channels});
-    }
-    channels[channel][user] = "ses";
-    socket.join(channel);
-}
-var leaveChannel = function(user, channel) {
-}
-
-var count = 0;
-io.on('connection', function(socket) {
-    console.log(channels);
-    count++;
-    var user = 'anon' + count;
-    socket.emit('hello', {channels: channels});
-    socket.on('join', function(data, fn) {
-        if(data.room) {
-            try {
-                joinChannel(user, data.room, socket);
-            }
-            catch(err){
-                console.log(err);
-            }
-        }
-        fn('joined');
-    });
-    socket.on('leave', function(data) {
-        if(data.room) {
-            try {
-                socket.leave(data.room);
-            }
-            catch(err){
-                console.log(err);
-            }
-        }
-    });
-    socket.on('message', function(data) {
-        if(channels[data.room] && channels[data.room][user]){
-            io.to(data.room).emit(data.room, {user: user, message: data.message});
-        }
-        else {
-            console.log("error:");
-            console.log(data);
-        }
-    });
-    
-});
+io.on('connection', initCons);
 
 
 
