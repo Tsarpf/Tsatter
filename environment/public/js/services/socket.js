5b92e9eabdd864fc200222a54831a22f7a75245c
Teemu Sarapisto <tsarpf@gmail.com>
2015-02-13 22:25:48 +0200

    Clean up

--- environment/public/js/services/socket.js
+++ environment/public/js/services/socket.js
@@ -1,34 +1,6 @@
 angular.module('tsatter').factory('socket', function($rootScope) {
     var address = location.host;
     var socket = io(address);
-    return {
-        on: function(channel, callback) {
-            socket.on(channel, function () {
-                var args = arguments; 
-                $rootScope.$apply(function() {
-                    if(callback) {
-                        callback.apply(socket, args);
-                    }
-                });
-            });
-        },
-        emit: function(channel, data, callback) {
-            socket.emit(channel, data, function() {
-                var args = arguments; 
-                $rootScope.$apply(function() {
-                    if(callback) {
-                        callback.apply(socket, args);
-                    }
-                });
-            });
-        }
-    };
-});
-
-
-angular.module('tsatter').factory('socket', function($rootScope) {
-    var address = location.host;
-    var socket = io(address);
     socket.on('message', function(message) {
         console.log(message);
         $rootScope.$apply(function() {
@@ -36,15 +8,7 @@ angular.module('tsatter').factory('socket', function($rootScope) {
         });
     });
 
-    /*
-    socket.on('message', function() {
-
-    });
-    */
-
-    var listeners = {};
     return {
-
         joinChannel: function(channel) {
             //listeners[channelName] = listenersObj;
             socket.on(channel, function(data) {
