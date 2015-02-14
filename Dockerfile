493fecd706a6224821307360405a2bac518673d5
Teemu Sarapisto <tsarpf@gmail.com>
2015-02-15 01:10:59 +0200

    Start using bower

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM dockerfile/nodejs
 ADD environment/package.json /home/environment/
 WORKDIR /home/environment
 RUN npm install
-RUN npm install -g nodemon
+RUN npm install -g nodemon && npm install -g bower
+ADD environment/bower.json /home/environment/
+RUN bower install
 EXPOSE 3000 3000
 ADD environment/start.sh /home/environment/start.sh
 CMD bash -C 'start.sh'; bash;
--- environment/start.sh
+++ environment/start.sh
@@ -5,6 +5,13 @@ do
   sleep 1
 done
 echo "$(date) - connected successfully"
+mkdir -p src/public/libs/css
+mkdir -p src/public/libs/js
+cp bower_components/bootstrap/dist/css/bootstrap.min.css src/public/libs/css/
+cp bower_components/angular/angular.min.js src/public/libs/js/
+cp bower_components/angular-animate/angular-animate.min.js src/public/libs/js/
+cp bower_components/socket.io-client/socket.io.js src/public/libs/js/
+cp bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js src/public/libs/js/
 cd src
 npm test
 npm start
