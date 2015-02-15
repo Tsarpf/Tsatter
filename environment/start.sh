2a58a2eea20c4cf344fabb528e978ec1893423b7
Teemu Sarapisto <tsarpf@gmail.com>
2015-02-15 02:17:22 +0200

    Fixing version compatibilities after bower mess

--- environment/start.sh
+++ environment/start.sh
@@ -9,7 +9,9 @@ mkdir -p src/public/libs/css
 mkdir -p src/public/libs/js
 cp bower_components/bootstrap/dist/css/bootstrap.min.css src/public/libs/css/
 cp bower_components/angular/angular.min.js src/public/libs/js/
+cp bower_components/angular/angular.min.js.map src/public/libs/js/
 cp bower_components/angular-animate/angular-animate.min.js src/public/libs/js/
+cp bower_components/angular-animate/angular-animate.min.js.map src/public/libs/js/
 cp bower_components/socket.io-client/socket.io.js src/public/libs/js/
 cp bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js src/public/libs/js/
 cd src
