de1e49ffb25a19732b2bbc849304df4157e893be
Teemu Sarapisto <tsarpf@gmail.com>
2015-02-18 21:06:29 +0200

    Start serving real data from server routes

--- environment/server/routes.js
+++ environment/server/routes.js
@@ -22,62 +22,8 @@ module.exports = function(app) {
     app.get('/activity/', function(req, res, next) {
         var from = req.query.from;
         var to = req.query.to;
-        persistenceHandler.getActiveChannels(from, to, function(err, channels) {
-
+        persistenceHandler.getActiveChannels(from, to, function(err, results) {
+            res.json(results);
         });
-
-        var obj = [
-            {
-                messages: ['hello iamaboy anda message', 'tseke vaara', 'joujou'],
-                topic: 'sup',
-                imageUrl: 'http://i.imgur.com/dH4Ilnh.gif'
-            },
-            {
-                messages: ['mesitsuun'],
-                topic: 'topikkidesu',
-                imageUrl: 'http://i.imgur.com/voyE0oi.jpg?1'
-            },
-            {
-                messages: ['mesitsuun', 'jouko hainen', 'meikä läinen'],
-                topic: 'topikkidesu',
-                imageUrl: 'http://i.imgur.com/OCJ1HnW.jpg'
-            },
-            {
-                messages: ['mesitsuun', 'naalfdkgalökdjf'],
-                topic: 'topikkidesu',
-                imageUrl: 'http://i.imgur.com/JB0Fd3R.gif'
-            },
-            {
-                messages: ['aa long mssagea long mssagea long mssagea long mssagea long mssagea long mssagea long mssagea long mssagea long mssage long mssage', 'ses'],
-                topic: 'immatopic',
-                imageUrl: 'http://i.imgur.com/VEnIb5i.jpg'
-            },
-            {
-                messages: ['hello iamaboy anda message', 'tseke vaara', 'joujou'],
-                topic: 'sup',
-                imageUrl: 'http://i.imgur.com/dH4Ilnh.gif'
-            },
-            {
-                messages: ['mesitsuun'],
-                topic: 'topikkidesu',
-                imageUrl: 'http://i.imgur.com/voyE0oi.jpg?1'
-            },
-            {
-                messages: ['mesitsuun', 'jouko hainen', 'meikä läinen'],
-                topic: 'topikkidesu',
-                imageUrl: 'http://i.imgur.com/OCJ1HnW.jpg'
-            },
-            {
-                messages: ['mesitsuun', 'naalfdkgalökdjf'],
-                topic: 'topikkidesu',
-                imageUrl: 'http://i.imgur.com/JB0Fd3R.gif'
-            },
-            {
-                messages: ['aa long mssagea long mssagea long mssagea long mssagea long mssagea long mssagea long mssagea long mssagea long mssage long mssage', 'ses'],
-                topic: 'immatopic',
-                imageUrl: 'http://i.imgur.com/VEnIb5i.jpg'
-            },
-        ];
-        res.json(obj);
     });
 };
