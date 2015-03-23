10c723f8b2a94bb8f0144eb7d4cb5afa8307caef
Jami Karvanen <jami@plop.fi>
2015-03-23 21:59:20 +0200

    Implement grunt build task which concatenates, minifies & revisions files

--- environment/Gruntfile.js
+++ environment/Gruntfile.js
@@ -1,11 +1,11 @@
 module.exports = function(grunt) {
 
     grunt.file.expand('../node_modules/grunt-*/tasks').forEach(grunt.loadTasks);
-    //grunt.loadNpmTasks('grunt-bower-task');
-    //grunt.loadNpmTasks('grunt-wiredep');
 
     grunt.initConfig({
+
         pkg: grunt.file.readJSON('package.json'),
+
         bower: {
             install: {
                 options: {
@@ -20,6 +20,7 @@ module.exports = function(grunt) {
                 }
             }
         },
+
         wiredep: {
             options: {
                     directory: 'public/libs/',
@@ -31,9 +32,75 @@ module.exports = function(grunt) {
                     'app/views/layout.jade'
                 ]
             }
+        },
+
+
+        useminPrepare: {
+            jade: ['dist/app/views/*.jade'],
+            options: {
+                dest: 'dist',
+                patterns: {
+                    jade: require('usemin-patterns').jade
+                }
+            }
+        },
+        usemin:{
+            jade: ['dist/app/views/*.jade'],
+            options: {
+                patterns: {
+                    jade: require('usemin-patterns').jade
+                },
+                blockReplacements: {
+                    css: function (block) {
+                        return "link(rel='stylesheet', href='" + block.dest.replace('public/', '') + "')";
+                    },
+                    js: function (block) {
+                        return "script(src='" + block.dest.replace('public/', '') + "')";
+                    }
+                }
+            }
+        },
+        filerev: {
+            stylesheets: {
+                src: 'dist/assets/css/styles.min.css'
+            },
+            js: {
+                src: 'dist/js/*.js'
+            }
+        },
+        copy:{
+            app: {
+                expand: true,
+                src: ['app/**/*.js', 'app/**/*.jade'],
+                dest: 'dist/'
+            },
+            server: {
+                expand: true,
+                src: ['server/**/*.js'],
+                dest: 'dist/'
+            },
+            runjs: {
+                src: ['run.js'],
+                dest: 'dist/'
+            }
+        },
+        clean: {
+            dist: ['dist']
         }
+
     });
 
     grunt.registerTask('deps',['bower','wiredep']);
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'copy',
+        'useminPrepare',
+        'concat:generated',
+        'cssmin:generated',
+        'uglify:generated',
+        'filerev',
+        'usemin'
+    ]);
 
 };
