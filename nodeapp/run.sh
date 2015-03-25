af50406ec649c51407c758e24e19e27d8e860045
Teemu Sarapisto <tsarpf@gmail.com>
2015-03-25 23:13:48 +0200

    Cleaning up the mess...

--- nodeapp/run.sh
+++ nodeapp/run.sh
@@ -1,5 +1,6 @@
-docker build -t tsatter/web:latest .
-docker stop webapp
-docker rm -f webapp
-docker run -p 3000:3000 --name webapp -v $(pwd)/environment:/home/nonroot/environment/src --link db:db_1 --link inspirk:ircserver -t -i tsatter/web
+echo "I need super user privileges because docker"
+sudo docker build -t tsatter/web:latest .
+sudo docker stop webapp
+sudo docker rm -f webapp
+sudo docker run -p 3000:3000 --name webapp -v $(pwd)/environment:/home/nonroot/environment/src --link db:db_1 --link inspirk:ircserver -t -i tsatter/web
 
