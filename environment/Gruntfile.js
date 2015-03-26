ff15441cc063aeeddbf0fb3ed70004502aade9c7
Teemu Sarapisto <tsarpf@gmail.com>
2015-03-26 19:56:16 +0200

    Using html instead of jade now works

--- environment/Gruntfile.js
+++ environment/Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
             },
             task: {
                 src: [
-                    'app/views/index.html',
+                    'app/views/index.html'
                 ]
             }
         }
