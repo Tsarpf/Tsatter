d2797e3e384d73f685059e43c67066ce0a2dc989
Teemu Sarapisto <tsarpf@gmail.com>
2015-03-27 23:15:04 +0200

    Start serving statics from port 3000 using node again for faster development

--- nodeapp/run.sh
+++ nodeapp/run.sh
@@ -1,4 +1,3 @@
-echo "I need super user privileges because docker"
 sudo docker build -t tsatter/web:latest .
 sudo docker rm -f nodeapp 
-sudo docker run --name nodeapp -v $(pwd)/environment:/home/nonroot/environment/src --link db:db_1 --link inspirk:ircserver -t -d tsatter/web
+sudo docker run --name nodeapp -p 3000:3000 -v $(pwd)/environment:/home/nonroot/environment/src --link db:db_1 --link inspirk:ircserver -t -d tsatter/web
