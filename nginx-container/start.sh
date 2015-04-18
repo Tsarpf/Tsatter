83b82bdb073dc59db8cf3ce4cc7ae6f8eca0267f
Teemu Sarapisto <tsarpf@gmail.com>
2015-04-18 14:05:31 -0400

    Use self uploaded nginx image because dockerfile/nginx isn't available for some reason

--- nginx-container/start.sh
+++ nginx-container/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo docker rm -f nginx
-sudo docker run -d -p 80:80 -v $(pwd)/sites-enabled:/etc/nginx/sites-enabled  -v $(pwd)/logs:/var/log/nginx -v $(pwd)/../nodeapp/environment/dist:/var/www/dist --name nginx --link nodeapp:nodeapp dockerfile/nginx
+sudo docker run -d -p 80:80 -v $(pwd)/sites-enabled:/etc/nginx/sites-enabled  -v $(pwd)/logs:/var/log/nginx -v $(pwd)/../nodeapp/environment/dist:/var/www/dist --name nginx --link nodeapp:nodeapp tsarpf/nginx
 
 #docker run -d -p 80:80 -v <sites-enabled-dir>:/etc/nginx/sites-enabled -v <certs-dir>:/etc/nginx/certs -v <log-dir>:/var/log/nginx -v <html-dir>:/var/www/html dockerfile/nginx
 
