0a9b8388c0533c33b72f4947d09e35ea1a98ebf5
Teemu Sarapisto <tsarpf@gmail.com>
2015-05-12 23:25:05 +0300

    Already refactoring the almost whole thing

--- nodeapp/environment/public/js/services/infiniteMessages.js
+++ nodeapp/environment/public/js/services/infiniteMessages.js
@@ -5,21 +5,20 @@
 
 
 angular.module('tsatter').factory('infiniteMessages', ['$q', '$http', '$timeout', function($q, $http, $timeout) {
-    var rev = 0;
-
-    var first = true;
-    var bufferSize = 10;
-    var index0 = null;
-    var adapter;
-
-    var EOF = null;
-    var bottomLoaded = false;
-
-    var cache = [];
     return function(obj) {
+        var rev = 0;
+
+        var first = true;
+        var bufferSize = 10;
+        var index0 = null;
+        var adapter;
+
+        var EOF = null;
+        var bottomLoaded = false;
+
+        var cache = [];
         adapter = obj.adapter;
         function getData(index, count, callback) {
-            bufferSize  = count;
             var timedOutGetData = function(index, count, success) {
                 return function() {
                     getData(index, count, success);
@@ -32,24 +31,35 @@ angular.module('tsatter').factory('infiniteMessages', ['$q', '$http', '$timeout'
                 return;
             }
 
-            if(first && obj.linkOffset === null) {
-                //Then just request the last x messages
-                noLinkGetLast().then(function(data) {
-                    callback(data);
-                });
-            }
-            else if(first && obj.linkOffset >= 0) {
-                //We want to show the linked message
-                getAroundLink().then(function(data) {
-                    callback(data);
-                }, function(err) {
-                    //Linked message not found, show last x messages
-                    console.log(err);
-                    noLinkGetLast().then(function(data) {
-                        callback(data);
+            if(first) {
+                first = false;
+                if(obj.linkOffset === null) {
+                    //Then just request the last x messages
+                    requestLastMessages(count).then(function(data) {
+                        return callback(data);
+                    });
+                }
+                else if(obj.linkOffset >= 0) {
+                    //We want to show the linked message
+                    getAroundLink(count).then(function(data) {
+                        return callback(data);
+                    }, function(err) {
+                        //Linked message not found, show last x messages
+                        console.log(err);
+                        requestLastMessages(count).then(function(data) {
+                            return callback(data);
+                        })
                     })
-                })
+                }
+                return;
             }
+
+
+
+            //get messages reqIndex, count
+            //index0 should be set by now
+            requestMessages()
+
 
             /*
             var params = {
@@ -81,15 +91,38 @@ angular.module('tsatter').factory('infiniteMessages', ['$q', '$http', '$timeout'
             */
         }
 
-        function getAroundLink(link) {
+        function getMessages(index, count) {
+            /*
+            var deferred = $q.defer();
+
+            if(EOF === null) {
+                requestMessages(obj.channel, index, count).then(function(data) {
+
+                });
+                return deferred.promise;
+            }
+            var arr = [];
+            for(var i = index; i < index + count && i < EOF; i++) {
+                if(!cache[i]) {
+
+                }
+
+                arr.push(cache[i]);
+            }
+
+            deferred.resolve(arr);
+            return deferred.promise;
+            */
+        }
+
+        function getAroundLink(count) {
            var deferred = $q.defer();
 
-            first = false;
-            var index = obj.linkOffset - bufferSize / 2;
+            var index = obj.linkOffset - count / 2;
             if(index < 0) {
                 index = 0;
             }
-            getMessages(obj.channel, index, bufferSize).then(function(data) {
+            requestMessages(obj.channel, index, count).then(function(data) {
                 if(data.length === 0) {
                     deferred.reject('did not find such message, showing last messages from channel');
                     return;
@@ -97,29 +130,18 @@ angular.module('tsatter').factory('infiniteMessages', ['$q', '$http', '$timeout'
 
                 for(var i = 0; i < data.length; i++) {
                     if(data[i].idx === obj.linkOffset) {
+                        if(data.length < count) {
+                            EOF = data[data.length - 1].idx;
+                            bottomLoaded = true;
+                        }
                         data[i].class = 'single-message-highlighted';
                         obj.currentlyHighlighted = data[i];
-                        insertCache(data);
                         deferred.resolve(data);
                         return;
                     }
                 }
 
                 deferred.reject('did not find such message, showing last messages from channel');
-            });
-
-            return deferred.promise;
-        }
-
-        function noLinkGetLast() {
-            var deferred = $q.defer();
-
-            first = false;
-            getLastMessages(obj.channel).then(function(data) {
-                EOF = data[data.length - 1].idx;
-                bottomLoaded = true;
-                insertCache(data);
-                deferred.resolve(data);
             });
 
             return deferred.promise;
@@ -147,6 +169,79 @@ angular.module('tsatter').factory('infiniteMessages', ['$q', '$http', '$timeout'
             }
         }
 
+
+
+        function requestMessages(index, count) {
+            var deferred = $q.defer();
+
+            var reqIndex = index0 + index;
+            var reqCount = count;
+            if(reqIndex + count <= 0) {
+                deferred.resolve([]);
+                return deferred.promise;
+            }
+
+            var remainder = 0;
+            if(reqIndex < 0) {
+                remainder = -reqIndex;
+                reqCount = count - remainder;
+                reqIndex = 0;
+            }
+
+            $http.get('/backlog/', {
+                params: {
+                    channel: obj.channel,
+                    index: reqIndex,
+                    count: reqCount
+                }
+            }).success(function(data) {
+                if(data.length === 0) {
+                    EOF = cache.length - 1;
+                    bottomLoaded = true;
+                }
+                if(data.length < count) {
+                    //Cant just set EOF here because we could be requesting for example -3 to 7 which was then translated to 0-7 for our purposes and then count would be 6
+                    EOF = data[data.length - 1].idx;
+                    bottomLoaded = true;
+                }
+                insertCache(data);
+                deferred.resolve(data);
+            }).error(function(data) {
+                deferred.reject('error when requesting data!');
+            });
+
+            return deferred.promise;
+        }
+
+        function requestLastMessages(count) {
+            var deferred = $q.defer();
+
+            $http.get('/backlog/', {
+                params: {
+                    channel: obj.channel,
+                    count: count
+                }
+            }).success(function(data) {
+                if(data.length === 0) {
+                    index0 = 0;
+                    EOF = 0;
+                    bottomLoaded = true;
+                    return deferred.resolve(data);
+                }
+
+                index0 = data[0].idx;
+                EOF = data[data.length - 1].idx;
+                bottomLoaded = true;
+                insertCache(data);
+                deferred.resolve(data);
+            }).error(function(data) {
+                console.log('error');
+                deferred.reject('error!');
+            });
+
+            return deferred.promise;
+        }
+
         return {
             get: getData,
             incrementRevision: incrementRevision,
@@ -155,22 +250,4 @@ angular.module('tsatter').factory('infiniteMessages', ['$q', '$http', '$timeout'
         }
     };
 
-    function getLastMessages(channel, count) {
-        return $http.get('/backlog/', {
-            params: {
-                channel: channel,
-                count: count
-            }
-        });
-    }
-
-    function getMessages(channel, index, count) {
-        return $http.get('/backlog/', {
-            params: {
-                channel: channel,
-                index: index,
-                count: count
-            }
-        }) ;
-    }
 }]);
