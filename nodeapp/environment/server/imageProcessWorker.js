03da123dd014c5e032cc0602cbe98a5a1c2ea70d
Teemu Sarapisto <tsarpf@gmail.com>
2015-06-06 04:49:45 -0400

    Surround image processing in a try catch for now

--- nodeapp/environment/server/imageProcessWorker.js
+++ nodeapp/environment/server/imageProcessWorker.js
@@ -27,10 +27,9 @@ var thumbnailDimensions = {
     height: 500
 };
 
-//message object fields: url, channel
 process.on('message', function(msg) {
     download(msg.url, function(err, obj) {
-        if(err) {
+        if(err || !obj) {
             return;
         }
 
@@ -54,124 +53,135 @@ process.on('message', function(msg) {
 });
 
 
-//obj fields: path, shouldProcess, extension
 var minifyImage = function(obj, callback) {
-    var origPath = imagesPath + obj.filename;
-    var path = origPath + '.' + obj.type;
-    var filename = obj.filename + '.' + obj.type;
-    switch(obj.type) {
-        case 'jpg':
-            gm(origPath)
-                .interlace('Plane')
-                .quality(85)
-                .resize(thumbnailDimensions.width, thumbnailDimensions.height + '>')
-                .noProfile()
-                .write(path, function(err) {
-                    if(err) {
-                        console.log('err');
-                        console.log(err);
-                    }
-                    else {
-                        callback(filename);
-                    }
-                });
-            break;
-        case 'png':
-            gm(origPath)
-                .colors(256)
-                .quality(90)
-                .bitdepth(8)
-                .resize(thumbnailDimensions.width, thumbnailDimensions.height + '>')
-                .noProfile()
-                .write(path, function(err) {
-                    if(err) {
-                        console.log('err');
-                        console.log(err);
-                    }
-                    else {
-                        callback(filename);
-                    }
-                });
-            break;
-        case 'gif':
-            gm(origPath + '[0]')
-                .resize(thumbnailDimensions.width, thumbnailDimensions.height + '>')
-                .noProfile()
-                .write(path, function(err) {
-                    if(err) {
-                        console.log('err');
-                        console.log(err);
-                    }
-                    else {
-                        callback(filename);
-                    }
-                });
-            break;
+    try {
+        var origPath = imagesPath + obj.filename;
+        var path = origPath + '.' + obj.type;
+        var filename = obj.filename + '.' + obj.type;
+        switch(obj.type) {
+            case 'jpg':
+                gm(origPath)
+                    .interlace('Plane')
+                    .quality(85)
+                    .resize(thumbnailDimensions.width, thumbnailDimensions.height + '>')
+                    .noProfile()
+                    .write(path, function(err) {
+                        if(err) {
+                            console.log('err');
+                            console.log(err);
+                        }
+                        else {
+                            callback(filename);
+                        }
+                    });
+                break;
+            case 'png':
+                gm(origPath)
+                    .colors(256)
+                    .quality(90)
+                    .bitdepth(8)
+                    .resize(thumbnailDimensions.width, thumbnailDimensions.height + '>')
+                    .noProfile()
+                    .write(path, function(err) {
+                        if(err) {
+                            console.log('err');
+                            console.log(err);
+                        }
+                        else {
+                            callback(filename);
+                        }
+                    });
+                break;
+            case 'gif':
+                gm(origPath + '[0]')
+                    .resize(thumbnailDimensions.width, thumbnailDimensions.height + '>')
+                    .noProfile()
+                    .write(path, function(err) {
+                        if(err) {
+                            console.log('err');
+                            console.log(err);
+                        }
+                        else {
+                            callback(filename);
+                        }
+                    });
+                break;
+        }
+    }
+    catch(e) {
+        console.log('minifying crashed');
+        console.log(e);
     }
 };
 
 var download = function(url, callback) {
-    var type = '';
-    var filename = getName();
-    var filepath = imagesPath + filename;
-    var size;
-    var stream = request({
-        url: url,
-        method: "HEAD"
-    }, function(err, headRes) {
-        if(err) {
-            console.log(err);
-            console.log('request fail');
-            return callback(err);
-        }
-        size = headRes.headers['content-length'];
-        if (size > maxSize) {
-            console.log('skipped due to header content-length');
-            console.log('Resource size exceeds limit (' + size + ')');
-            return callback(null, {thumbnail: imageTooBig, shouldProcess: false});
-        }
-        size = 0;
-        var file = fs.createWriteStream(filepath);
-
-        var res = request({ url: url});
-        var checkType = true;
-        res.on('data', function(data) {
-            size += data.length;
-
-            if(checkType && size >= 4) {
-                var hex = data.toString('hex', 0, 4);
-                for(var key in fileTypes) {
-                    if(fileTypes.hasOwnProperty(key)) {
-                        if(hex.indexOf(fileTypes[key]) === 0) {
-                            type = key;
-                            checkType = false;
-                            break;
-                        }
-                    }
-                }
-                if(!type) {
-                    res.abort();
-                    fs.unlink(filepath);
-                    console.log('not an image');
-                    return callback('not an image');
-                }
+    try {
+        var type = '';
+        var filename = getName();
+        var filepath = imagesPath + filename;
+        var size;
+        var stream = request({
+            url: url,
+            method: "HEAD"
+        }, function(err, headRes) {
+            if(err) {
+                console.log(err);
+                console.log('request fail');
+                return callback(err);
             }
-
+            size = headRes.headers['content-length'];
             if (size > maxSize) {
-                console.log('aborted due to actual size too big');
-                console.log('Resource stream exceeded limit (' + size + ')');
-
-                res.abort(); // Abort the response (close and cleanup the stream)
-                fs.unlink(filepath); // Delete the file we were downloading the data to
+                console.log('skipped due to header content-length');
+                console.log('Resource size exceeds limit (' + size + ')');
                 return callback(null, {thumbnail: imageTooBig, shouldProcess: false});
             }
-        }).pipe(file);
-        res.on('end', function() {
-            if(type && size < maxSize) {
-                callback(null, {filename: filename, shouldProcess: true, type: type});
-            }
-        })
-    });
+            size = 0;
+            var file = fs.createWriteStream(filepath);
+
+            var res = request({ url: url});
+            var checkType = true;
+            res.on('data', function(data) {
+                size += data.length;
+
+                if(checkType && size >= 4) {
+                    var hex = data.toString('hex', 0, 4);
+                    for(var key in fileTypes) {
+                        if(fileTypes.hasOwnProperty(key)) {
+                            if(hex.indexOf(fileTypes[key]) === 0) {
+                                type = key;
+                                checkType = false;
+                                break;
+                            }
+                        }
+                    }
+                    if(!type) {
+                        res.abort();
+                        fs.unlink(filepath);
+                        console.log('not an image');
+                        return callback('not an image');
+                    }
+                }
+
+                if (size > maxSize) {
+                    console.log('aborted due to actual size too big');
+                    console.log('Resource stream exceeded limit (' + size + ')');
+
+                    res.abort(); // Abort the response (close and cleanup the stream)
+                    fs.unlink(filepath); // Delete the file we were downloading the data to
+                    return callback(null, {thumbnail: imageTooBig, shouldProcess: false});
+                }
+            }).pipe(file);
+            res.on('end', function() {
+                if(type && size < maxSize) {
+                    callback(null, {filename: filename, shouldProcess: true, type: type});
+                }
+            })
+        });
+    }
+    catch(e) {
+        console.log('downloading crashed');
+        console.log(e);
+    }
 };
 
 var getName = function() {
